a2a24a5eccdcd328243b2dbba3234322154eb94a
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-18 14:52:10 -0500

    completed first GET request

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const { Router } = require("express");
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
@@ -6,4 +7,33 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/",(req, res) =>{
+    db.select('*')
+    .from('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+    .catch(handleError)
+})
+
+server.get("/:id",(req, res) =>{
+    
+})
+
+server.post("/",(req, res) =>{
+    
+})
+
+server.put("/:id",(req, res) =>{
+    
+})
+
+server.delete("/:id",(req, res) =>{
+    
+})
+
+function handleError(error, res){
+    console.log("error", error)
+    res.status(500).json({message: error.message})
+}
 module.exports = server;
